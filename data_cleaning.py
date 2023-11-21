b76650e23a6351e1535d4f4058b130d90a49010f
pili-zhangqiu <p.zhangqiu@gmail.com>
2023-11-21 00:56:06 +0000

    fix(clean-table): Solve bugs with data cleaning class and abstract methods
    - Bugs solved include: missed NULL parameters to filter, incorrect phones assigned NULL value but not removed, etc
    - Abstracted methods for clarity

--- data_cleaning.py
+++ data_cleaning.py
@@ -22,28 +22,47 @@ class DataCleaning():
         and rows filled with the wrong information.
         '''
         # Remove all rows containing NULL values
-        df = df.dropna()
+        df = self.clean_nulls(df)
 
         # Remove all rows with errors in dates
-        df['date_of_birth'] = pd.to_datetime(df['date_of_birth'], errors='coerce').dt.date  # Remove wrong date formatting
+        df = self.clean_dates(df)
+
+        # Remove rows with incorrect formatting in first_name and last_name
+        df = self.clean_names(df)
+
+        # Remove rows with wrong phone_number formatting
+        df = self.clean_phones(df)
+
+        return df
+    
+    def clean_nulls(self, df: pd.DataFrame) -> pd.DataFrame:
+        # Remove rows with any value being NULL or NaN
+        df = df.dropna()
+
+        # Remove rows with any value being a string 'NULL'
+        df = df[~df.isin(['NULL', 'null', 'Null']).any(axis=1)]
+
+        return df
+
+    def clean_dates(self, df: pd.DataFrame) -> pd.DataFrame:
+        # Remove rows with wrong date formatting
+        df['date_of_birth'] = pd.to_datetime(df['date_of_birth'], errors='coerce').dt.date  
         df['join_date'] = pd.to_datetime(df['join_date'], errors='coerce').dt.date
 
-        df = df[~(df['join_date'] < df['date_of_birth'])]   # Remove rows where joint_date is after date_of_birth
+        # Remove rows where joint_date is after date_of_birth
+        df = df[~(df['join_date'] < df['date_of_birth'])]
 
-        current_date = date.today()                         # Remove rows where dates are after the current date
+        # Remove rows where dates are after the current date
+        current_date = date.today()
         df = df[~(df['date_of_birth'] > current_date)]
         df = df[~(df['join_date'] > current_date)]
 
+        return df
+    
+    def clean_names(self, df:pd.DataFrame) -> pd.DataFrame:
         # Remove rows with incorrect formatting in first_name and last_name
         df = df[(df['first_name'].apply(self.is_valid_name))]
         df = df[(df['last_name'].apply(self.is_valid_name))]
-
-        # TODO: Doesnt seem to do anything
-        # Remove rows with wrong phone_number formatting
-        regex_expression = '^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$'
-        df.loc[~df['phone_number'].str.match(regex_expression), 'phone_number'] = np.nan         # For every row  where the Phone column does not match our regular expression, replace the value with NaN
-
-        print(df)
 
         return df
 
@@ -57,6 +76,14 @@ class DataCleaning():
                 return False
         return True
 
+    def clean_phones(self, df:pd.DataFrame) -> pd.DataFrame:
+        # TODO: Doesn't seem to do anything
+        # Remove rows with wrong phone_number formatting
+        regex_expression = '^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$'
+        df.loc[~df['phone_number'].str.match(regex_expression), 'phone_number'] = np.nan         # For every row  where the Phone column does not match our regular expression, replace the value with NaN
+        df = df.dropna()
+
+        return df
 
 if __name__ == '__main__':
     connector = DatabaseConnector('db_creds_aws.yaml')
