8b79d910a0b22fbd6c161eaf8723a473251c1777
pili-zhangqiu <p.zhangqiu@gmail.com>
2023-11-22 10:39:58 +0000

    feat(main): Implement main logic for card data extraction, cleaning and upload from PDF

--- main.py
+++ main.py
@@ -7,20 +7,35 @@ from data_cleaning import DataCleaning
 from data_extraction import DataExtractor
 
 if __name__ == '__main__':
-    # Create connection to the database
+    '''
+    Extract, Process and Upload Data
+    '''
+    # Create connection to the databases
     connector_aws = DatabaseConnector('db_creds_aws.yaml')
+    connector_local = DatabaseConnector('db_creds_local.yaml')
+
+    # Prepare instances of extraction and cleaning utility classes
+    extractor = DataExtractor()
+    cleaner = DataCleaning()
     
+    # ------------------ User Data ------------------
     # Extract table data
     read_table_name = 'legacy_users'
-    extractor = DataExtractor()
-    df = extractor.read_rds_table(connector_aws, read_table_name)
+    df_user = extractor.read_rds_table(connector_aws, read_table_name)
 
     # Clean the data
-    cleaner = DataCleaning()
-    df = cleaner.clean_user_data(df)
+    df_user = cleaner.clean_user_data(df_user)
     
-    # Upload dataframe as table to the database
-    connector_local = DatabaseConnector('db_creds_local.yaml')
-    upload_table_name = 'dim_users'
-    connector_local.upload_to_db(df, upload_table_name)
+    # Upload dataframe as table to the local PostgreSQL database
+    connector_local.upload_to_db(df_user, 'dim_users')
     
+    # ------------------ Card Data ------------------
+    # Extract table data from PDF
+    pdf_url = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+    df_card = extractor.retrieve_pdf_data(pdf_url)
+
+    # Clean the data
+    df_card = cleaner.clean_card_data(df_card)
+    
+    # Upload dataframe as table to the local PostgreSQL database
+    connector_local.upload_to_db(df_card, 'dim_card_details')
