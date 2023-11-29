b7ccd3a69deac9b5c1232f67feae61fba725dfa5
pili-zhangqiu <p.zhangqiu@gmail.com>
2023-11-29 06:04:01 +0000

    feat(get-api): Implement methods to get all store details

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import yaml
 from tabula.io import read_pdf
 
 from database_utils import DatabaseConnector
@@ -9,7 +10,13 @@ class DataExtractor():
     Utility class to extract data from multiple sources, including: CSV files, APIs and S3 buckets.
     '''
     def __init__(self):
-        pass
+        '''
+        Initialise DataExtractor class and load class variables
+        '''
+        self._api_stores_headers = {
+            'x-api-key': self.retrieve_creds('db_creds_aws_api.yaml')['X_API_KEY']
+            }
+        self._api_stores_base_url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod'
 
     def read_rds_table(self, db_connector: DatabaseConnector, table_name: str) -> pd.DataFrame:
         '''
@@ -32,23 +39,81 @@ class DataExtractor():
         
         return df 
     
-    def list_number_of_stores(self, endpoint_url: str, headers: dict) -> int:
+    def retrieve_creds(self, filepath: str) -> dict:
+        '''
+        Returns credential data from a YAML file given its path and key name.
+
+        Parameters:
+        ----------
+        filepath: str
+            Filepath to the credentials YAML
+
+        Returns:
+        -------
+        creds: dict
+            Authentication credentials from YAML file
+        '''
+        # Parse YAML
+        with open(filepath, 'r') as file:
+            creds = yaml.safe_load(file)
+        return creds
+    
+    def list_number_of_stores(self) -> int:
         '''
         Returns the number of stores to extract. It should take in the number of stores endpoint and header dictionary as an argument.
         '''
         # Send a GET request to the API
+        endpoint_url = f'{self._api_stores_base_url}/number_stores'
+        headers = self._api_stores_headers
+
         response = requests.get(endpoint_url, headers=headers)
 
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             # Access the response data as JSON
             data = response.json()
-            return data
+            num_stores = int(data['number_stores'])
+            print(f'Total number of stores is {num_stores}')
+            return num_stores
 
         # If the request was not successful, print the status code and response text
         else:
             print(f"Request failed with status code: {response.status_code}")
-            print(f"Response Text: {response.text}")   
+            print(f"Response Text: {response.text}")
+            raise requests.exceptions.RequestException("Request failed")
+    
+    def retrieve_store_data(self, store_id: int) -> pd.DataFrame:
+        # Send a GET request to the API
+        endpoint_url = f'{self._api_stores_base_url}/store_details/{store_id}'
+        headers = self._api_stores_headers
+
+        response = requests.get(endpoint_url, headers=headers)
+
+        # Check if the request was successful (status code 200). If so, return data.
+        if response.status_code == 200:
+            return pd.json_normalize(response.json())
+
+        # If the request was not successful, print the status code and response text
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            print(f"Response Text: {response.text}")
+            raise requests.exceptions.RequestException("Request failed")
+        
+    def retrieve_stores_data(self) -> pd.DataFrame:
+        '''
+        Extracts and returns all the stores from the API in a pandas Dataframe format.
+        '''
+        # Get number of stores from API
+        num_stores = self.list_number_of_stores()
+
+        # Loop through all stores and save the data
+        stores_data = []
+        for store_id in range(1,num_stores):
+            store_data = self.retrieve_store_data(store_id)
+            print(store_data)
+            stores_data.append(store_data)
+
+        return stores_data
 
 if __name__ == '__main__':
     connector = DatabaseConnector('db_creds_aws_rds.yaml')
