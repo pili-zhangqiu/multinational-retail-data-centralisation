698bfc65afdfbb4134a7dcd7fabac9ea41702bb9
pili-zhangqiu <p.zhangqiu@gmail.com>
2023-12-11 01:06:09 +0000

    feat(cast): Cast the columns of dim_users_table to the correct data types
    Change the data types to correspond to:
    - first_name --> VARCHAR(255)
    - last_name --> VARCHAR(255)
    - date_of_birth  --> DATE
    - country_code --> VARCHAR(N)
    - user_uuid --> UUID
    - join_date --> DATE

---
+++ sql_casts/cast_dim_users_table.sql
@@ -0,0 +1,8 @@
+ALTER TABLE dim_users
+
+ALTER COLUMN first_name TYPE varchar(255),
+ALTER COLUMN last_name TYPE varchar(255),
+ALTER COLUMN date_of_birth TYPE date,
+ALTER COLUMN country_code TYPE varchar(3),
+ALTER COLUMN user_uuid TYPE uuid USING user_uuid::uuid,
+ALTER COLUMN join_date TYPE date;
