6b2a6206cd325285408e504ab68821f093f6e47e
pili-zhangqiu <p.zhangqiu@gmail.com>
2023-12-15 12:29:22 +0000

    feat(sql-no-stores): Add query to get number of stores per country

---
+++ sql_analysis/stores_per_country.sql
@@ -0,0 +1,5 @@
+-- Get the total number of stores in each country, from highest to lowest
+SELECT country_code AS country, COUNT (*) AS total_no_stores 
+FROM dim_store_details
+GROUP BY country_code
+ORDER BY total_no_stores DESC;
