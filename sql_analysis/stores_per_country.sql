f6ef0e4a4d037d838f78a8aeebf591d857fcb19f
pili-zhangqiu <p.zhangqiu@gmail.com>
2023-12-15 13:24:40 +0000

    file: Autoformat file

--- sql_analysis/stores_per_country.sql
+++ sql_analysis/stores_per_country.sql
@@ -1,5 +1,6 @@
 -- Get the total number of stores in each country, from highest to lowest
-SELECT country_code AS country, COUNT (*) AS total_no_stores 
+SELECT country_code AS country,
+    COUNT (*) AS total_no_stores
 FROM dim_store_details
 GROUP BY country_code
 ORDER BY total_no_stores DESC;
